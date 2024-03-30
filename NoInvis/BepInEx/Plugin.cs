8364c5019472e1b009f373a64e472e598845ee79
Benjamin M <reallybaddeveloper@gmail.com>
2024-03-30 03:30:59 -0400

    Add files via upload

---
+++ NoInvis/BepInEx/Plugin.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BoplFixedMath;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+
+namespace NoInvis
+{
+    [BepInPlugin("com.rbdev.NoInvis", "No Invis", "1.0.0")]
+    [BepInProcess("BoplBattle.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("Plugin No Invis is loaded!");
+            Harmony harmony = new Harmony("com.rbdev.NoInvis");
+
+            MethodInfo original = AccessTools.Method(typeof(Invisibility), "SetAlphaBasedOnPlayerInvisibiltyTime");
+            MethodInfo patch = AccessTools.Method(typeof(Patches), "Patch");
+
+            harmony.Patch(original,new HarmonyMethod(patch));
+        }
+    }
+    public class Patches {
+        public static void Patch(ref Player player) {
+            player.isInvisible = false;
+        }
+    }
+}
