13432c8fa2dc39c5e5fea5322deaf0db56875243
Benjamin M <reallybaddeveloper@gmail.com>
2024-03-30 04:14:27 -0400

    Add files via upload

---
+++ ScrewJumping/BepInEx/Plugin.cs
@@ -0,0 +1,34 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using BoplFixedMath;
+using HarmonyLib;
+using System.IO;
+using System.Reflection;
+using UnityEngine.Yoga;
+
+namespace ScrewJumping
+{
+    [BepInPlugin("com.rbdev.ScrewJumping", "ScrewJumping", "1.0.0")]
+    [BepInProcess("BoplBattle.exe")]
+
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("Plugin ScrewJumping is loaded!");
+            Harmony harmony = new Harmony("com.rbdev.ScrewJumping");
+
+            MethodInfo original = AccessTools.Method(typeof(PlayerPhysics), "Jump");
+            MethodInfo patch = AccessTools.Method(typeof(Patches), "Patch");
+
+            harmony.Patch(original,new HarmonyMethod(patch));
+        }
+    }
+    public class Patches {
+        public static void Patch(PlayerPhysics __instance) {
+            __instance.jumpStrength = (Fix)(-30L);
+        }
+    }
+}
